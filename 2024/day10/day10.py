cb63de2a3c5eb27396016ce699a4fc82843ea1b9
Matthew Chandler <48606413+matthew-chandler@users.noreply.github.com>
2024-12-26 11:57:13 -0800

    2024 day10 extra

--- 2024/day10/day10.py
+++ 2024/day10/day10.py
@@ -41,47 +41,76 @@ def part1():
 
 def part2():
     with open("input.in") as file:
-        map = [[(int(c),[]) for c in line] for line in file.read().split("\n")]
+        map = [[[int(c),0] for c in line] for line in file.read().split("\n")]
 
+    # add 9s to queue
     q = queue.Queue()
     for i in range(len(map)):
         for j in range(len(map[0])):
             if map[i][j][0] == 9:
                 q.put((i,j))
-                map[i][j][1].append([])
+                map[i][j][1] = 1
 
-    import time
-
+    visited = set()
     while not q.empty():
         i,j = q.get()
+        if (i,j) in visited:
+            continue
+        else:
+            visited.add((i,j))
         val = map[i][j][0]
-        print(i,j,map[i][j][0],map[i][j][1])
-        print()
-        time.sleep(0.5)
-        # check left
+        score = map[i][j][1]
+
         if i > 0 and map[i-1][j][0] == val - 1:
             q.put((i-1,j))
-            for path in map[i][j][1]:
-                map[i-1][j][1].append(path+[(i,j)])
+            map[i-1][j][1] += score
         if i < len(map) - 1 and map[i+1][j][0] == val - 1:
             q.put((i+1,j))
-            for path in map[i][j][1]:
-                map[i+1][j][1].append(path+[(i,j)])
+            map[i+1][j][1] += score
         if j > 0 and map[i][j-1][0] == val - 1:
             q.put((i,j-1))
-            for path in map[i][j][1]:
-                map[i][j-1][1].append(path+[(i,j)])
+            map[i][j-1][1] += score
         if j < len(map[0]) - 1 and map[i][j+1][0] == val - 1:
             q.put((i,j+1))
-            for path in map[i][j][1]:
-                map[i][j+1][1].append(path+[(i,j)])
+            map[i][j+1][1] += score
     
+    # count up the number of paths per 0
     sum = 0
     for i in range(len(map)):
         for j in range(len(map[0])):
             if map[i][j][0] == 0:
-                sum += len(map[i][j][1])
+                sum += map[i][j][1]
     print(sum)
 
 part1()
 part2()
+
+def part2v2():
+    with open("input.in") as file:
+        map = [[int(c) for c in line] for line in file.read().split("\n")]
+
+    q = queue.Queue()
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 9:
+                q.put((i,j))
+
+    sum = 0
+    while not q.empty():
+        i,j = q.get()
+        val = map[i][j]
+        if val == 0:
+            sum += 1
+            continue
+        if i > 0 and map[i-1][j] == val - 1:
+            q.put((i-1,j))
+        if i < len(map) - 1 and map[i+1][j] == val - 1:
+            q.put((i+1,j))
+        if j > 0 and map[i][j-1] == val - 1:
+            q.put((i,j-1))
+        if j < len(map[0]) - 1 and map[i][j+1] == val - 1:
+            q.put((i,j+1))
+
+    print(sum)
+
+# part2v2()
